d603c57e44ef26933c112c7cde1a9cc1d1631dd1
William E <38293619+SirWilliamE@users.noreply.github.com>
2019-02-11 14:01:22 -0500

    Create everydayClasses.sql
    School Scheduling Database: Show a list of classes that meet Monday through Friday and start at 1 pm.

---
+++ everydayClasses.sql
@@ -0,0 +1,7 @@
+SELECT * FROM schoolschedulingexample.classes
+WHERE MondaySchedule = 1
+AND TuesdaySchedule = 1
+AND WednesdaySchedule = 1
+AND ThursdaySchedule = 1
+AND FridaySchedule = 1
+AND StartTime = '13:00:00';
